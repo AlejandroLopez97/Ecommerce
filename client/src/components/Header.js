4ce682c550ac5e5a2071e823b7b4d781ae442577
Alejandro Lopez <alejooliveros29@gmail.com>
2023-10-27 17:16:49 -0500

    Se agregan iconos, se exportan desde index en assets, se comienza a dar forma al header de la pagina

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from 'react';
+import {logoDark} from "../assets/index";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <div className='w-full h-20 bg-white border-b-[1px] border-b-gray-800'>
 
-export default Header
+      <div className='max-w-screen-xl h-full mx-auto flex items-center justify-between'>
+        <section>
+          <img className='w-28' src={logoDark} alt='logoDark'></img>
+        </section>
+
+        <section>
+        <ul>
+          <li>Inicio</li>
+          <li>Paginas</li>
+          <li>Tienda</li>
+          <li>Elementos</li>
+          <li>Blog</li>
+        </ul>
+        </section>        
+      </div>
+     
+    </div>
+  );
+};
+
+export default Header;
