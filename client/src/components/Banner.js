c2c50c4e576cdbae7fed2b3291ad2bde3906c2d4
Alejandro Lopez <alejooliveros29@gmail.com>
2023-10-30 20:24:33 -0500

    Se le va dando estilo al slider creado en el componente Banner y se instala react-icons para utilizar los iconos de flechas en el slider

--- client/src/components/Banner.js
+++ client/src/components/Banner.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {HiArrowRight, HiArrowLeft} from 'react-icons/hi';
 
 const Banner = () => {
  const data = [
@@ -8,15 +9,49 @@ const Banner = () => {
     "https://amazonproone.vercel.app/static/media/img1.efb3d39101f7ef77d616.jpg",
   ];
   return (
-    <div className='w-full h-auto'>
-      <div>
-        <div>
+    <div className='w-full h-auto overflow-x-hidden'>
+      {/* contenedor de imagenes y botones */}
+      <div className='w-screen h-[650px] relative'>
+        {/* contenedor de imagenes del slider */}
+        <div className='w-[400vw] h-full flex'>
           <img 
             className='w-screen h-full object-cover'
             src={data[0]} 
-            alt='ImgOne' 
+            alt='ImgOne'
+            loading='priority'
+          />
+          <img 
+            className='w-screen h-full object-cover'
+            src={data[1]} 
+            alt='ImgTwo'
+          />
+          <img 
+            className='w-screen h-full object-cover'
+            src={data[2]} 
+            alt='ImgThree'
+          />
+          <img 
+            className='w-screen h-full object-cover'
+            src={data[3]} 
+            alt='ImgFour'
           />
         </div>
+        {/* fin imagenes slider */}
+
+        {/* contenedor botones slider */}
+        <div className='absolute w-fit left-0 right-0 mx-auto flex gap-8 bottom-44'>
+          <div className='w-14 h-12 border-[1px] border-gray-700 flex items-center
+          justify-center hover:cursor-pointer hover:bg-gray-700 hover:text-white
+          active:bg-gray-900 duration-300'>
+            <HiArrowLeft /> {/*flecha izquierda importada desde react-icons*/}
+          </div>
+          <div className='w-14 h-12 border-[1px] border-gray-700 flex items-center
+          justify-center hover:cursor-pointer hover:bg-gray-700 hover:text-white
+          active:bg-gray-900 duration-300'>
+            <HiArrowRight /> {/*flecha derecha importada desde react-icons*/}
+          </div>
+        </div>
+        {/* fin botones slider */}
       </div>
     </div>
   )
