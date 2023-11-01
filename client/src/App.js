c4a2b84a80902c8d4f71fa50e49bcfe9d3edcd87
Alejandro Lopez <alejooliveros29@gmail.com>
2023-11-01 18:49:20 -0500

    Se crea la pagina Cart, para el carrito de compras y se configura el react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { createRef } from "react";
 import Header from "./components/Header";
 import Home from "./pages/Home";
 import Footer from "./components/Footer";
+import {
+  createBrowserRouter,
+  Outlet,
+  RouterProvider,
+  ScrollRestoration,
+} from 'react-router-dom';
+
+const Layout = ()=>{
+  return (
+    <div>
+      <Header />
+      <Outlet />
+      <Footer />
+    </div>
+  );
+}
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children:[
+      {
+        path: "/",
+        element: <Home />,
+      }
+    ]
+  }
+]);
 
 function App() {
   return (
