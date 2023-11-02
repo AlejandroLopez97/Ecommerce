b7f87b2ebfdb6be12b15b975365acfd508536891
Alejandro Lopez <alejooliveros29@gmail.com>
2023-11-01 20:21:33 -0500

    Se termina el componente de productos y se crea el componente donde se mostrara el detall del producto

--- client/src/components/ProductsCard.js
+++ client/src/components/ProductsCard.js
@@ -1,8 +1,48 @@
 import React from 'react';
+import { BsArrowRight } from 'react-icons/bs';
 
-function ProductsCard() {
+const  ProductsCard = ({product}) => {
+
   return (
-    <div>ProductsCard</div>
+    <main className='group relative'>
+      <section className='w-full h-96 cursor-pointer overflow-hidden'>
+        <img
+          className='w-full h-full object-cover group-hover:scale-110 duration-500'
+          src={product.image}
+          alt='productImg'
+        />
+      </section>
+      <section className='w-full border-[1px] px-2 py-4'>
+        <section className='flex justify-between items-center'>
+          <div>
+           <h2 className='font-titleFont text-base font-bold'>{product.title.substring(0, 15)}</h2>
+          </div>
+         <div className='flex justify-end gap-2 relative overflow-hidden w-28 text-sm'>
+            <div className='flex gap-2 transform group-hover:translate-x-24
+              transition-transform duration-500'>
+              <p className='line-through text-gray-500'>${product.oldPrice}</p>
+              <p className='font-semibold'>${product.price}</p>
+            </div>
+            <p className='absolute z-20 w-[100px] text-gray-500 hover:text-gray-900 flex 
+              items-center gap-1 top-0 text-sm transform -translate-x-32 group-hover:translate-x-0
+              transition-transform cursor-pointer duration-500'>
+              Agregar
+              <span><BsArrowRight/></span>
+            </p>
+          </div>
+        </section>
+        <section>
+          <p>{product.category}</p>
+        </section>
+        <section className='absolute top-4 right-0'>
+          {product.isNew && (
+            <p className='bg-black text-white font-semibold font-titleFont px-6 py-1'>
+              Oferta
+            </p>
+          )}
+        </section>
+      </section>
+    </main>
   )
 };
 
