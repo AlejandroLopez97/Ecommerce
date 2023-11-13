7942c366afb9b2b594b23a0124e53373df9477f1
Alejandro Lopez <alejooliveros29@gmail.com>
2023-11-13 16:06:07 -0500

    Se agregan constantes en bazarSlice para manejar los estados de incrementar items, decrementar items, resetear el carrito de compras y eliminar item

--- client/src/redux/bazarSlice.js
+++ client/src/redux/bazarSlice.js
@@ -11,6 +11,7 @@ export const bazarSlice = createSlice({
   reducers:{
     // funcionalidad para agregar al carrito de CompressionStream, donde valido sin son igual la accion o no para 
     // saber si incremento la cantidad o agrego otro producto al carrito de compras
+    //eliminar item, resetear carrito, incrementar items y drecementar items
     addToCart: (state, action)=>{
       const item = state.productData.find(
         (item) => item._id === action.payload._id
@@ -21,9 +22,41 @@ export const bazarSlice = createSlice({
       } else {
         state.productData.push(action.payload);
       }
-    }
+    },
+    deleteItem: (state, action) => {
+      state.productData = state.productData.filter(
+        (item) => item._id !== action.payload
+      );
+    },
+    resetCart: (state) =>{
+      state.productData = [];
+    },
+    increamentQuantity: (state, action) =>{
+      const item = state.productData.find(
+        (item) => item._id === action.payload._id
+      );
+      if (item) {
+        item.quantity++;
+      }
+    },
+    decrementQuantity: (state, action) =>{
+      const item = state.productData.find(
+        (item) => item._id === action.payload._id
+      );
+      if (item.quantity === 1){
+        item.quantity = 1;
+      }else{
+        item.quantity--;
+      }
+    },
   }
 });
 
-export const {addToCart} = bazarSlice.actions;
+export const {
+  addToCart,
+  deleteItem,
+  resetCart,
+  increamentQuantity,
+  decrementQuantity,
+} = bazarSlice.actions;
 export default bazarSlice.reducer;
