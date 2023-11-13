74fc451f02dd22e87084aca8e826c5d728b5fc00
Alejandro Lopez <alejooliveros29@gmail.com>
2023-11-13 16:27:04 -0500

    Se crea un componente para el manejo de los productos en la pagina cart, llamado CartItem.js

--- client/src/pages/Cart.js
+++ client/src/pages/Cart.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import {useSelector} from 'react-redux';
+import { cartImg } from '../assets';
+import CartItem from '../components/CartItem';
 
-function Cart() {
+const Cart = () => {
+  const productData = useSelector((state) => state.bazar.productData);
+  //console.log(productData);
   return (
-    <div>Cart</div>
-  );
-}
+    <div>
+      <img
+        className='w-full h-90 object-cover'
+        src='https://img.freepik.com/fotos-premium/fondo-viernes-negro-etiqueta-ventas-viernes-negro-sobre-fondo-rojo-etiqueta-venta-inscripcion-viernes-negro-sobre-fondo-rojo_256259-3236.jpg?w=826'
+        alt='CartImg'
+      />
+      <div>
+        <CartItem />
+      </div>
+    </div>
+  )
+};
 
 export default Cart;
